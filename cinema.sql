b84b809452e544f3f1dd28da0c656860a7ab29bf
MarcusHoang <71581489+nghiahoang43@users.noreply.github.com>
2022-06-16 05:13:16 +0000

    Thu, Jun 16, 2022, 5:13 AM +00:00

--- cinema.sql
+++ cinema.sql
@@ -153,7 +153,69 @@ END //
 DELIMITER ;
 
 
+
 /* Mock database */
+/* Create User */
+CALL sign_up('user1', 'user1@gmail.com', '123');
+CALL sign_up('user2', 'user2@gmail.com', '123');
+CALL sign_up('user3', 'user3@gmail.com', '123');
+CALL sign_up('user4', 'user4@gmail.com', '123');
+CALL sign_up('user5', 'user5@gmail.com', '123');
+CALL sign_up('user6', 'user6@gmail.com', '123');
+
+/* Create Movie */
 CALL add_movie('LIGHTYEAR', 105, 'https://wallis-digital-cdn.app.vista.co/media/entity/get/FilmPosterGraphic/HO00001408?width=300&height=450&allowPlaceHolder=false', 'The sci-fi action-adventure presents the definitive origin story of Buzz Lightyear—the hero who inspired the toy—introducing the legendary Space Ranger who would win generations of fans.');
 CALL add_movie('JURASSIC WORLD: DOMINION', 147, 'https://wallis-digital-cdn.app.vista.co/media/entity/get/FilmPosterGraphic/HO00001152?width=300&height=450&allowPlaceHolder=false', 'This June, experience the epic conclusion to the Jurassic era as two generations unite for the first time. Chris Pratt and Bryce Dallas Howard are joined by Oscar®-winner Laura Dern, Jeff Goldblum, and Sam Neill in Jurassic World Dominion, a bold, timely, and breathtaking new adventure that spans the globe.');
 CALL add_movie('DOCTOR STRANGE IN THE MULTIVERSE OF MADNESS', 126, 'https://wallis-digital-cdn.app.vista.co/media/entity/get/FilmPosterGraphic/HO00000975?width=300&height=450&allowPlaceHolder=false', 'In Marvel Studios’ Doctor Strange in the Multiverse of Madness, the MCU unlocks the Multiverse and pushes its boundaries further than ever before. Journey into the unknown with Doctor Strange, who, with the help of mystical allies both old and new, traverses the mind-bending and dangerous alternate realities of the Multiverse to confront a mysterious new adversary.');
+
+/* Create Room */
+CALL add_room(1);
+CALL add_room(2);
+CALL add_room(3);
+
+/* Create Seat Room 1 */
+CALL add_seat(1, 1);
+CALL add_seat(1, 2);
+CALL add_seat(1, 3);
+CALL add_seat(1, 4);
+CALL add_seat(1, 5);
+CALL add_seat(1, 6);
+CALL add_seat(1, 7);
+CALL add_seat(1, 8);
+CALL add_seat(1, 9);
+CALL add_seat(1, 10);
+
+/* Create Seat Room 2 */
+CALL add_seat(2, 1);
+CALL add_seat(2, 2);
+CALL add_seat(2, 3);
+CALL add_seat(2, 4);
+CALL add_seat(2, 5);
+CALL add_seat(2, 6);
+CALL add_seat(2, 7);
+CALL add_seat(2, 8);
+CALL add_seat(2, 9);
+CALL add_seat(2, 10);
+
+/* Create Seat Room 3 */
+CALL add_seat(3, 1);
+CALL add_seat(3, 2);
+CALL add_seat(3, 3);
+CALL add_seat(3, 4);
+CALL add_seat(3, 5);
+CALL add_seat(3, 6);
+CALL add_seat(3, 7);
+CALL add_seat(3, 8);
+CALL add_seat(3, 9);
+CALL add_seat(3, 10);
+
+/* Create Booking */
+CALL add_booking('1', '2', '2022-06-20', '12:30:00');
+CALL add_booking('1', '3', '2022-06-12', '17:10:00');
+CALL add_booking('2', '2', '2022-06-12', '09:00:00');
+CALL add_booking('2', '2', '2022-06-21', '11:30:00');
+CALL add_booking('2', '1', '2022-06-23', '12:30:00');
+CALL add_booking('3', '1', '2022-06-14', '19:45:00');
+CALL add_booking('3', '3', '2022-06-17', '10:10:00');
+
+CALL 
